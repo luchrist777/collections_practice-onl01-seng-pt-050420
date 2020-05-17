8f3ebade8262c2bb2967e770d13ca7b470a22fbf
Luis Leandro <luchrist777@gmail.com>
2020-05-17 00:26:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,96 +1,54 @@
-Collections Practice
+def sort_array_asc(integers)
+  integers.sort
+end
 
-Finish the first collections assignment if you haven't already.
-Wrap each of these problems in a method, you can also write a test for each method if you'd like to practice testing.  All solutions should be generic and work for any "case" but sometimes a sample case is given.
-These problems should get progressively more difficult.
-Return true if every element of the tools array starts with an "r" and false otherwise.
-  tools = ["ruby", "rspec", "rails"]
-Create a new array from the captain_planet array with all the elements that contain the letter "a".  
-  captain_planet = ["earth", "fire", "wind", "water", "heart"]
-Identify the first element in the stuff array that begins with the letters "wa".
-  stuff = ["candy", :pepper, "wall", :ball, "wacky"]
-Identify all the elements in the stuff array that begins with the letters "wa".
-  stuff = ["candy", :pepper, "wall", :ball, "wacky"]
-Remove anything that's not a string from an array.
-Hint: Use the method "class"  "blake".class
-Change the third letter of all strings in an array.  Your solution should work for arrays that have mixed types of objects inside it.
-Count the number of times each word appears in a string and store that data in a hash that has the word as the key and the count as the value.
-  string = "the flatiron school is better than general assembly"
-Count the number of times each hash appears in the array, remove any duplicates and add a :count key to each hash with the number of times it appears.
-  [{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}]
-    becomes
-  [{:name => "blake", :count => 2}, {:name => "ashley", :count => 1}]
-Take two arrays of hashes and merge the first names and last names into a new array of hashes where each hash has all information about itself.
-[
-       {
-        :first_name => "blake"
-    },
-       {
-        :first_name => "ashley"
-    }
-]
-and
-[
-       {
-         "blake" => {
-            :awesomeness => 10,
-                 :height => "74",
-              :last_name => "johnson"
-        },
-        "ashley" => {
-            :awesomeness => 9,
-                 :height => 60,
-              :last_name => "dubs"
-        }
-    }
-]
-becomes
-[
-       {
-         :first_name => "blake",
-        :awesomeness => 10,
-             :height => "74",
-          :last_name => "johnson"
-    },
-       {
-         :first_name => "ashley",
-        :awesomeness => 9,
-             :height => 60,
-          :last_name => "dubs"
-    }
-]
-Return all hashes that have a value of "cool" for the :temperature key.
-[
-        {
-               :name => "ashley",
-        :temperature => "sort of cool"
-    },
-        {
-               :name => "blake",
-        :temperature => "cool"
-    }
-]
-Convert the nested data structure from it's current structure
-{
-  "flatiron school bk" => {
-    :location => "NYC",
-    :price => "free"
-  },
-  "dev boot camp" => {
-    :location => "SF"
-    :price => "a million dollars"
-  },
-  "dev boot camp chicago" => {
-    :location => "Chicago",
-    :price => "half a million dollars"
-  },
-  "general assembly" => {
-    :location => "NYC",
-    :price => "too much"
-  },
-  "some school in SF" => {
-    :location => "SF",
-    :price => "your soul"
-  }
-}
-to a structure that organizes the schools by location.
+def sort_array_desc(integers)
+  integers.sort {|first_num, second_num| second_num <=> first_num}
+end
+
+def sort_array_char_count(strings)
+  strings.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(strings)
+  strings[1], strings[2] = strings[2], strings[1]
+  return strings
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  return array
+end
+
+def reverse_array(integers)
+  new_array = integers.reverse
+  new_array
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  kesha
+end
+
+def find_a(array)
+  array.select{|string| string.start_with?("a")}
+end
+
+def sum_array(integers)
+  integers.inject{|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
+  end
+end
